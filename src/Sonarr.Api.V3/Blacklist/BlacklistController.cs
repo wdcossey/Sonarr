8aae30c3f0d742707d1c8832d02cb99f43c46aca
William D Cossey <wdcossey@github.com>
2021-11-05 13:56:58 +0000

    Sonarr.Api.V3: Added `BlacklistController`. Removed unused/legacy Modules. Removed `Newtonsoft.Json` reference.

---
+++ src/Sonarr.Api.V3/Blacklist/BlacklistController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Blocklisting;
+using Sonarr.Api.V3.Blocklist;
+using Sonarr.Http.Attributes;
+
+namespace NzbDrone.Api.V3.Blacklist
+{
+    [ApiController]
+    [SonarrApiRoute("blacklist", RouteVersion.V3)]
+    public class BlacklistController : BlocklistController
+    {
+        public BlacklistController(BlocklistService blocklistService) 
+            : base(blocklistService) { }
+    }
+}
